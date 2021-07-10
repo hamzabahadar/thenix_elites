91131004eb7f0490e7ebdb5df4212a108ac89f4d
hamzabahadar <hamzabahadar2000@gmail.com>
2021-07-10 15:43:34 +0500

    first commit

---
+++ app/src/main/java/com/example/thenixelites/FullBody_Intermediate.java
@@ -0,0 +1,14 @@
+package com.example.thenixelites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FullBody_Intermediate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_body_intermediate);
+    }
+}
---
+++ app/src/main/java/com/example/thenixelites/UpperBody_Intermediate.java
@@ -0,0 +1,14 @@
+package com.example.thenixelites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpperBody_Intermediate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upper_body_intermediate);
+    }
+}
